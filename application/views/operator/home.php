c4774cdcb69fb1672c1cff122b56b5865eeccd46
Ivan Zaqqa <velocitysages@outlook.com>
2021-07-05 08:02:03 +0700

    sync users img with database for operator

--- application/views/operator/home.php
+++ application/views/operator/home.php
@@ -9,7 +9,7 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-white-600 small"><?= $users['name']; ?></span>
-                <img class="img-profile rounded-circle" src="<?= base_url() ?>/assets/img/undraw_profile.svg">
+                <img class="img-profile rounded-circle" src="<?= base_url('assets/img/users/') . $users['image']; ?>">
             </a>
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
                 <a class="dropdown-item" href="#">
