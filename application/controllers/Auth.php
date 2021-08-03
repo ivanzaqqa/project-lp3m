49f14a37ac8b749139a9692874795f880775d7ad
Ivan Zaqqa <velocitysages@outlook.com>
2021-08-03 10:05:09 +0700

    fix codes

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,27 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
-
-    // public function __construct()
-    // {
-    //     parent::__construct();
-    //     $this->load->library('form_validation');
-    // }
-
-    // public function index()
-    // {
-    //     $this->form_validation->set_rules('username', 'Username', 'trim|required');
-    //     $this->form_validation->set_rules('password', 'Password', 'trim|required');
-    //     if ($this->form_validation->run() == false) {
-    //         $this->load->view('templates/auth_header');
-    //         $this->load->view('auth/login');
-    //         $this->load->view('templates/auth_footer');
-    //     } else {
-    //         //validation success
-    //         $this->proccess();
-    //     }
-    // }
-
     public function index() {
         check_already_login_dosen();
         check_already_login_operator();
@@ -55,42 +34,6 @@ class Auth extends CI_Controller
             }
         }
     }
-
-    // private function _login()
-    // {
-    //     $username = $this->input->post('username');
-    //     $password = $this->input->post('password');
-
-    //     $users = $this->db->get_where('users', ['username' => $username])->row_array();
-
-    //     //jika usernya ada
-    //     if ($users) {
-    //         //jika usernya aktif
-
-    //         //cek password
-    //         if (hash_verified($password, $users['password'])) {
-    //             $data = [
-    //                 'username' => $users['username'],
-    //                 'id_role' => $users['id_role']
-    //             ];
-    //             $this->session->set_userdata($data);
-    //             //check id role
-    //             if ($users['id_role'] == 1) {
-    //                 redirect('dosen');
-    //             } elseif ($users['id_role'] == 2) {
-    //                 redirect('operator');
-    //             } else {
-    //                 echo 'Anda Bukan Dosen atau Operator!';
-    //             }
-    //         } else {
-    //             $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Password salah!</div>');
-    //             redirect('auth');
-    //         }
-    //     } else {
-    //         $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Operator dengan username tersebut tidak ada!</div>');
-    //         redirect('auth');
-    //     }
-    // }
 
     public function logout()
     {
