0c91a77df8eff83f4eed56accfe7a5b9e98faa96
Ivan Zaqqa <velocitysages@outlook.com>
2021-11-06 13:53:23 +0700

    delete lp3m logo from copyright and fix tutorial page in dosen

--- application/views/dosen/tutorial.php
+++ application/views/dosen/tutorial.php
@@ -8,8 +8,18 @@
             </div>
         </div>
         <div class="card-body">
-            <iframe width="600" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY">
-            </iframe>
+            <h4>PENELITIAN</h4>
+            <iframe src="https://drive.google.com/file/d/1uY9mcDbhso9P7pavG12G41aynSmx8CpB/preview" width="640" height="480" allow="autoplay"></iframe>
+        </div>
+
+        <div class="card-body">
+            <h4>PENGABDIAN</h4>
+            <iframe src="https://drive.google.com/file/d/1AH_tOYEi4QsHQ4C-7PgtcuLUcRDaR0z6/preview" width="640" height="480" allow="autoplay"></iframe>
+        </div>
+
+        <div class="card-body">
+            <h4>INSENTIF PUBLIKASI</h4>
+            <iframe src="https://drive.google.com/file/d/1-8xR7ottKO5ZuAw_pQGCNKvPdEh09RVG/preview" width="640" height="480" allow="autoplay"></iframe>
         </div>
     </div>
 
--- application/views/templates/auth_footer.php
+++ application/views/templates/auth_footer.php
@@ -1,7 +1,7 @@
 <div class="navbar">
     <div class="container my-auto">
         <div class="copyright my-auto">
-            <span style="font-size: 12px; position: fixed; left: 0; bottom: 0; width: 100%; background-color: rgb(103,0,153); color: white; text-align: center;">Copyright &copy; Sistem Management Hibah Internal Universitas Kadiri <img class="mt-1 mb-1" src="<?= base_url() ?>/assets/img/default/lp3m.jpeg" width="80px;" alt=""></span>
+            <span style="font-size: 12px; position: fixed; left: 0; bottom: 0; width: 100%; background-color: rgb(103,0,153); color: white; text-align: center;">Copyright &copy; Sistem Management Hibah Internal Universitas Kadiri</span>
         </div>
     </div>
 </div>
